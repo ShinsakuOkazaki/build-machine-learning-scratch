43c3bd81b3bff0ef09af342f2bbba44835602bbf
sinsakuokazaki <golisaku4639@gmail.com>
2019-08-23 18:56:54 -0400

    added svm

--- svm_hard.py
+++ svm_hard.py
@@ -13,6 +13,11 @@ from operator import itemgetter
 
 
 class SVC:
+    
+#     def __init__(self):
+#         self.a_ = None
+#         self.w_ = None
+#         self.w0_ = None
     # X is (n, p), y is (p, 1)
     def fit(self, X, y, selections=None):
         # a is (n, 1)
@@ -25,8 +30,10 @@ class SVC:
         # indices is (n, 1)
         indices = np.arange(X.shape[0])
         
+        count = 0
         while True:
-            
+            count += 1
+            print("Iteration:", count)
             #get y multiple derivative of function
             
             # ydf is (p, 1)
@@ -38,12 +45,12 @@ class SVC:
             #when y = -1 or a >0
             i = int(min(iydf[(y < 0) | (a > 0)], 
                              key=itemgetter(1))[0])
-            
+            print("i:", i)
             #get index of maximum value of derivative of function
             #when y = 1 or a > 0
             j = int(max(iydf[(y > 0) | (a > 0)], 
                              key=itemgetter(1))[0])
-            
+            print("j:", j)  
             #check if we need to update
             if ydf[i] >= ydf[j]:
                 break
@@ -58,7 +65,7 @@ class SVC:
             
             # get new ai
             ai = ((1 - y[i] * y[j] + y[i] * np.dot(X[i, :] - X[j, :], X[j,:] * ay2 - ayx2)) \
-                                                        / ((X[i, :] - X[j, :])**2).sum())
+                                                        / ((X[i] - X[j])**2).sum())
             
             # if ai is smaller than 0, the optimal is ai = 0
             if ai < 0:
@@ -75,7 +82,7 @@ class SVC:
             
             # update ay and ayx with only values changed in this iteration
             ay += y[i]*(ai - a[i]) + y[j]*(aj - a[j])
-            ayx += y[i]*(ai - a[i])*X[i, :] + y[j](aj - a[j])*X[j,:]
+            ayx += y[i]*(ai - a[i])*X[i, :] + y[j]*(aj - a[j])*X[j,:]
             
             #if new ai is equal to last ai,do not assign new ai
             # same for aj, because if ai does not change aj neither.
@@ -90,9 +97,15 @@ class SVC:
         
         # culculate w and w0 from a, y and x only where a's elements are not 0
         ind = a != 0.
+        print("ind", ind)
+        # store w (ind.sum(), p)
         self.w_ = ((a[ind] * y[ind]).reshape(-1, 1) \
-                   * X[ind, :].sum(axis=0))
+                   * X[ind, :]).sum(axis=0)
+        print("X[ind,:]", X[ind, :].shape)
+        print("self.w_", self.w_.shape)
+        # store w0 (1, ind.sum())
         self.w0_ = (y[ind] - np.dot(X[ind, :], self.w_)).sum() / ind.sum()
+        print("self.w0_", self.w0_)
         
     def predict(self, X):
         return np.sign(self.w0_ + np.dot(X, self.w_))
